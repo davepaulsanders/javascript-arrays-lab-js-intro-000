64c6e6f5d2d5c832e6f5889936a564120c7d5f3f
David P Sanders <davepaulsanders@gmail.com>
2021-03-26 19:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var kittenstwo = kittens.concat(name);
+  var kittenstwo = kittens.concat(name)
   return kittenstwo
 }
