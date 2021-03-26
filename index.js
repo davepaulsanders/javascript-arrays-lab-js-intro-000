e3bb5498f135c419973857117c5b04eb4064cd34
David P Sanders <davepaulsanders@gmail.com>
2021-03-26 19:33:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name){
 kittens = kittens.concat(name);
 return kittens
-} //define your array here
+}
 
-// Add your functions and code here
+function destructivelyPrependKitten(name){
+  
+}
